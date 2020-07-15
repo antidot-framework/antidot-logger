b9dd57906d69048fb7c90e88bdb1aa55ee26782a
kpicaza <kpicaza.sound@gmail.com>
2020-07-15 18:44:49 +0200

    add missing comments and typehits to validate phpstan in the newer version

--- src/Container/MonologFactory.php
+++ src/Container/MonologFactory.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Antidot\Logger\Container;
 
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use WShafer\PSR11MonoLog\MonologFactory as BaseFactory;
 
 class MonologFactory extends BaseFactory
 {
-    public function __invoke(ContainerInterface $container, string $name = 'default')
+    public function __invoke(ContainerInterface $container, string $name = 'default'): LoggerInterface
     {
         self::$channelName = $name;
         return parent::__invoke($container);
